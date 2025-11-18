0cd5675603fe71b5f9208d3cbf1239719f4e6985
Mihir More <mihirmore.25@gmail.com>
2025-11-18 12:28:13 +0530

    Add user & foodpartner model schema with mongoose

---
+++ backend/src/models/foodpartner.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const foodPartnerSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const foodPartnerModel = mongoose.model(
+    "foodpartner",
+    foodPartnerSchema
+);
